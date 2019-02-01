478368b75c7ff11e6a45a485d2f25621f298a25e
Kitipat Teachasupinun <Kitipat_teacha@outlook.co.th>
2019-02-01 15:52:23 +0700

    test_nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,15 @@ import Profile from './containers/Profile'
 class App extends Component {
   render() {
     return (
+      
       <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </nav>
+          
         <header id="header-background" className="one" />
 				<ul id="a">
 					<li><a className="active" href="#home">Home</a></li>
