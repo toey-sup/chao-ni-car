fec254b7d83914438ba17a0b8314cf10a052e76b
phpunch <31317741+phpunch@users.noreply.github.com>
2019-02-01 14:00:01 +0700

    Add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Profile from './containers/Profile'
-
+import {Route} from 'react-router-dom'
 class App extends Component {
   render() {
     return (
       <div>
         <Profile />
+        <Route path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/result" component={ResultPage} />
       </div>
     )
   }
