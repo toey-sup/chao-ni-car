193d92497738395721c3791c1ab9318a1f74f179
Satasuk Viparksinlapin <satasuk@gmail.com>
2019-02-01 23:37:29 +0700

    create dummy data

--- frontend/src/components/CarCard/CarCards.js
+++ frontend/src/components/CarCard/CarCards.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import { Col,Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import CarCard from './CarCard';
 
 const carCards = (props) => {
     //props.cars แล้ว map ไปเป็น card แต่ละใบ
+    const dummyData = [{
+        title: "car1",
+        description: "xxxasfjapownceyrtvwnoperuvnwpoutvnpoewr"
+    }, {
+        title: "car2",
+        description: "xxxasfjapownceyrtvwnoperuvnwpoutvnpoewr"
+    }, {
+        title: "car3",
+        description: "xxxasfjapownceyrtvwnoperuvnwacfjvopnervropoutvnpoewr"
+    }, {
+        title: "car4",
+        description: "Not thing"
+    }, {
+        title: "car5",
+        description: "xxxasfjapownceyrtvwnoperuvnwacfjvopnervropoutvnpoewr"
+    }, {
+        title: "car6",
+        description: "xxxasfjapownceyrtvwnoperuvnwacfjvopnervropoutvnpoewr"
+    }].map((item) => {
+        return <Col xs={12} sm={4} md={3}><CarCard key={item.tile} title={item.title} description={item.description} /></Col>
+    })
     return (
         <Row>
-            <Col sm={3}><CarCard title="car1" description="XXXXXXXXXXXXXsfskdkgjhpjeorvtertuervgterjXXXXxx" /></Col>
-            <Col sm={3}><CarCard title="car2" description="XXXXXXXXXXXXXsfskdkgjhpjeorvtertuervgterjXXXXxx" /></Col>
-            <Col sm={3}><CarCard title="car3" description="XXXXXXXXXXXXXsfskdkgjhpjeorvtertuervgterjXXXXxx" /></Col>
-            <Col sm={3}><CarCard title="car4" description="XXXXXXXXXXXXXsfskdkgjhpjeorvtertuervgterjXXXXxx" /></Col>
-            <Col sm={3}><CarCard title="car5" description="XXXXXXXXXXXXXsfskdkgjhpjeorvtertuervgterjXXXXxx" /></Col>
-            <Col sm={3}><CarCard title="car6" description="XXXXXXXXXXXXXsfskdkgjhpjeorvtertuervgterjXXXXxx" /></Col>
-            
+            {dummyData}
         </Row>
     );
 }
