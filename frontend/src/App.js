c3064b0ad8f48d700e541d4a966b1e3d93902e87
phpunch <31317741+phpunch@users.noreply.github.com>
2019-02-01 15:30:15 +0700

    Add Homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,14 +12,6 @@ class App extends Component {
   render() {
     return (
       <div>
-        <header id="header-background" className="one" />
-				<ul id="a">
-					<li><a className="active" href="#home">Home</a></li>
-					<li><a href="#Create">Create New Activity</a></li>
-					<li><a href="#Attendance">Check Attendance</a></li>
-					<li><a href="#View">View Created Activity</a></li>
-					<li><a href="#History">History</a></li>
-				</ul>
         <Profile />
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
