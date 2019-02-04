ec46f8890580c7d38dbf4df6f53bf99ce3e13041
phpunch <nutacp_ryho@hotmail.com>
2019-02-04 22:07:38 +0700

    Remove test file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,8 @@ import AboutPage from './containers/AboutPage'
 import LoginPage from './containers/LoginPage'
 import ResultPage from './containers/ResultPage'
 
-import testPage from './containers/testPage'
-import authenticationPage from './containers/AuthenticationPage'
+//import testPage from './containers/testPage'
+//import authenticationPage from './containers/AuthenticationPage'
 
 class App extends Component {
   render() {
@@ -20,8 +20,6 @@ class App extends Component {
         <Route path="/about" component={AboutPage} />
         <Route path="/login" component={LoginPage} />
         <Route path="/result" component={ResultPage} />
-        <Route path="/test" component={testPage} />
-        <Route path="/authentication" component={authenticationPage} />
       </div>
     )
   }
