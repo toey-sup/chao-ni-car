110702328b1c1f5085ac4031c1297394d1e94361
Rvaipas <boss43031@gmail.com>
2019-02-05 17:50:13 +0700

    Clean code.

--- frontend/src/containers/AboutPage.js
+++ frontend/src/containers/AboutPage.js
@@ -5,17 +5,14 @@ import "./AboutPage.css";
 
 function MemberInfo(props){
     return(
-        <div>
-            <Row>
-                <Image src={props.imgSrc} roundedCircle className="MemberName"/>
-            </Row>
-            <Row>
-                <h3 className="MemberName">
-                    {props.name}
-                </h3>
-            </Row>
+        <div className="MemberBlock">
+            <div className="MemberImage" >
+                <Image src={props.imgSrc} roundedCircle/>
+            </div>
+            <h4 className="MemberName">
+                {props.name}
+            </h4>
         </div>
-        
     );
 }
 
@@ -27,38 +24,26 @@ class AboutPage extends Component {
                     <Row>
                         <h1 className="Header">Team Member</h1>
                     </Row>
-                    <Row className="MemberRow">
+                    <Row style={{paddingBottom: 20}}>
                         <Col>
-                            <div className="MemberBlock">
-                                <MemberInfo name="Member1" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
-                            </div>
+                            <MemberInfo name="Satasuk Viparksinlapin" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
                         </Col>
                         <Col>
-                            <div className="MemberBlock">
-                                <MemberInfo name="Member2" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
-                            </div>
+                            <MemberInfo name="Supanat Wongwiwatchai" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
                         </Col>
                         <Col>
-                            <div className="MemberBlock">
-                                <MemberInfo name="Member3" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
-                            </div>
+                            <MemberInfo name="Nuttapon Vittayaprechapon" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
                         </Col>
                     </Row>
-                    <Row className="MemberRow">
+                    <Row>
                         <Col>
-                            <div className="MemberBlock">
-                                <MemberInfo name="Member1" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
-                            </div>
+                            <MemberInfo name="Sahapol Tongsom" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
                         </Col>
                         <Col>
-                            <div className="MemberBlock">
-                                <MemberInfo name="Member2" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
-                            </div>
+                            <MemberInfo name="Kitipat Teachasupinun" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
                         </Col>
                         <Col>
-                            <div className="MemberBlock">
-                                <MemberInfo name="Member3" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
-                            </div>
+                            <MemberInfo name="Ravipas Plitnonkiat" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
                         </Col>
                     </Row>
                 </Container>
