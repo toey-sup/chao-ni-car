52536330d2c550f0bcfd1b610e376955ea807613
Rvaipas <boss43031@gmail.com>
2019-02-05 15:34:38 +0700

    Add background at AboutPage and fixed NavBar

--- frontend/src/containers/AboutPage.js
+++ frontend/src/containers/AboutPage.js
@@ -7,10 +7,10 @@ function MemberInfo(props){
     return(
         <div>
             <Row>
-                <Image src={props.imgSrc} roundedCircle />
+                <Image src={props.imgSrc} roundedCircle className="MemberName"/>
             </Row>
             <Row>
-                <h3>
+                <h3 className="MemberName">
                     {props.name}
                 </h3>
             </Row>
@@ -22,32 +22,46 @@ function MemberInfo(props){
 class AboutPage extends Component {
     render() {
         return (
-            <Container>
+            <Container className="Background">
                 <Row>
                     <h1 className="Header">Team Member</h1>
                 </Row>
                 <Row className="MemberRow">
                     <Col>
-                        <MemberInfo name="Member1" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
+                        <div className="MemberBlock">
+                            <MemberInfo name="Member1" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
+                        </div>
                     </Col>
                     <Col>
-                        <MemberInfo name="Member2" imgSrc = "holder.js/171x180"/>
+                        <div className="MemberBlock">
+                            <MemberInfo name="Member2" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
+                        </div>
                     </Col>
                     <Col>
-                        <MemberInfo name="Member3" imgSrc = "holder.js/171x180"/>
+                        <div className="MemberBlock">
+                            <MemberInfo name="Member3" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
+                        </div>
                     </Col>
                 </Row>
+                <div style={{padding: 20}}>
                 <Row>
                     <Col>
-                        <MemberInfo name="Member4" imgSrc = "holder.js/171x180"/>
+                        <div className="MemberBlock">
+                            <MemberInfo name="Member4" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
+                        </div>
                     </Col>
                     <Col>
-                        <MemberInfo name="Member5" imgSrc = "holder.js/171x180"/>
+                        <div className="MemberBlock">
+                            <MemberInfo name="Member5" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
+                        </div>
                     </Col>
                     <Col>
-                        <MemberInfo name="Member6" imgSrc = "holder.js/171x180"/>
+                        <div className="MemberBlock">
+                            <MemberInfo name="Member6" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
+                        </div>
                     </Col>
                 </Row>
+                </div>
             </Container>
         )
     }
