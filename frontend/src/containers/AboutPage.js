5bdfc459c2e152c58794acee64560ab9a7fb6299
Rvaipas <boss43031@gmail.com>
2019-02-05 17:08:15 +0700

    Improve AboutPage background.

--- frontend/src/containers/AboutPage.js
+++ frontend/src/containers/AboutPage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import Button from 'react-bootstrap/Button';
-import { Container, Row, Col , Image} from 'react-bootstrap';
+import { Container, Row, Col , Image, Card} from 'react-bootstrap';
 import "./AboutPage.css";
 
 function MemberInfo(props){
@@ -22,47 +22,47 @@ function MemberInfo(props){
 class AboutPage extends Component {
     render() {
         return (
-            <Container className="Background">
-                <Row>
-                    <h1 className="Header">Team Member</h1>
-                </Row>
-                <Row className="MemberRow">
-                    <Col>
-                        <div className="MemberBlock">
-                            <MemberInfo name="Member1" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className="MemberBlock">
-                            <MemberInfo name="Member2" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className="MemberBlock">
-                            <MemberInfo name="Member3" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
-                        </div>
-                    </Col>
-                </Row>
-                <div style={{padding: 20}}>
-                <Row>
-                    <Col>
-                        <div className="MemberBlock">
-                            <MemberInfo name="Member4" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className="MemberBlock">
-                            <MemberInfo name="Member5" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className="MemberBlock">
-                            <MemberInfo name="Member6" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
-                        </div>
-                    </Col>
-                </Row>
-                </div>
-            </Container>
+            <div className="Background" style={{paddingBottom: 10}}>
+                <Container>
+                    <Row>
+                        <h1 className="Header">Team Member</h1>
+                    </Row>
+                    <Row className="MemberRow">
+                        <Col>
+                            <div className="MemberBlock">
+                                <MemberInfo name="Member1" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div className="MemberBlock">
+                                <MemberInfo name="Member2" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div className="MemberBlock">
+                                <MemberInfo name="Member3" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row className="MemberRow">
+                        <Col>
+                            <div className="MemberBlock">
+                                <MemberInfo name="Member1" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div className="MemberBlock">
+                                <MemberInfo name="Member2" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div className="MemberBlock">
+                                <MemberInfo name="Member3" imgSrc = "https://cdn.pixabay.com/photo/2017/10/25/18/21/attribution-icon-2888829_960_720.png"/>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         )
     }
 }
