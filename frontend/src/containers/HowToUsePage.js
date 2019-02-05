a1ee81208af21085a4c6014faf41c0b68c7d1e53
rekcah BTC <donglovemom@gmail.com>
2019-02-05 16:24:51 +0700

    update howtousepage

--- frontend/src/containers/HowToUsePage.js
+++ frontend/src/containers/HowToUsePage.js
@@ -1,8 +1,5 @@
 
 import React, {Component} from 'react'
-import CarJumbotron1 from '../components/Jumbotron/Jumbotron1'
-import CarCards from '../components/CarCard/CarCards';
-//import Navbar from './components/Navbar/Navbar';
 
 class HowToUsePage extends Component {
     render() {
@@ -10,19 +7,19 @@ class HowToUsePage extends Component {
             <div>     
                 
             <div style={{ textAlign: 'center', paddingBottom: 15 }}>
-            <h1 class="display-3">How to use CAO-NI-CAR system?</h1>
+            <h1 class="display-1">How to use CAO-NI-CAR system?</h1>
             </div>
 
             <hr class="my-4"></hr>
             <p class="lead">1. You need to register system</p>
             <div style={{ textAlign: 'center', paddingBottom: 15 }}>
-            <img src="https://i.imgur.com/q4Q2mfc.png" width="700" height="700" />
+            <img src="https://i.imgur.com/q4Q2mfc.png" width="500" height="500" />
             </div>
 
             <hr></hr>
             <p class="lead">2. Fill your rental time</p>
             <div style={{ textAlign: 'center', paddingBottom: 15 }}>
-            <img src="https://i.imgur.com/8Vj3ngQ.jpg" width="1000" height="700" />
+            <img src="https://i.imgur.com/8Vj3ngQ.jpg" width="500" height="500" />
             </div>
 
             <hr></hr>
@@ -37,9 +34,23 @@ class HowToUsePage extends Component {
             <img src="https://i.imgur.com/xsSeasz.png" width="500" height="500" />
             </div>
             <hr ></hr>
+
+            <hr></hr>
+            <p class="lead">5. Make a payment</p>
+            <div style={{ textAlign: 'center', paddingBottom: 15 }}>
+            <img src="https://i.imgur.com/xsSeasz.png" width="500" height="500" />
+            </div>
+            <hr ></hr>
+
+            <hr></hr>
+            <p class="lead">6. Receive rented car</p>
+            <div style={{ textAlign: 'center', paddingBottom: 15 }}>
+            <img src="http://www.nezihturturizm.com.tr/images/kategoriler/big/MTU4NWE4YjQ3M2YyZDQ.jpg" width="500" height="500" />
+            </div>
+            <hr ></hr>
             
             </div>
-                
+        
         )
     }
 }
