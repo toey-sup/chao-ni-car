0225c76bd32e98873cc6e88190b3e95f43649f5c
rekcah BTC <donglovemom@gmail.com>
2019-02-05 09:25:19 +0700

    howtouse page

---
+++ frontend/src/containers/HowToUsePage.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import Button from 'react-bootstrap/Button';
+
+class HowToUsePage extends Component {
+    render() {
+        return (
+            <div>
+                <p>How to use this rental car system?</p>
+
+                <img src="http://www.carlogos.org/logo/Lamborghini-logo-640x550.jpg" width="200" height="200" />
+
+                <p>1. Login</p>
+
+                
+            </div>
+           
+        )
+    }
+}
+
+export default HowToUsePage
