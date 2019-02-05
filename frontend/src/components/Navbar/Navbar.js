6da7ae5e180baf33a56bbfd964c7afdad42b59db
Satasuk Viparksinlapin <satasuk@gmail.com>
2019-02-05 09:01:34 +0700

    fix navbar add logo add login and register button

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -8,22 +8,24 @@ import {
 } from "react-bootstrap";
 import { NavLink } from 'react-router-dom';
 import classes from "./Navbar.module.css";
+import logo from './logo.png';
 
 const NavbarComponent = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <NavLink className={classes.Header} to="/">Chao-Ni-Car</NavLink>
-
+      <NavLink className={classes.Header} to="/"><img src={logo} style={{ width: 100, marginTop: -7 }} />CAO-NI-CAR</NavLink>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <NavLink className={classes.NavLink} to="/">Rent A Car</NavLink>
-          <Nav.Link href="/">About</Nav.Link>
-          <Nav.Link href="#link">How to use</Nav.Link>
+          <NavLink className={classes.NavLink} to="/">About</NavLink>
+          <NavLink className={classes.NavLink} to="/">How to use</NavLink>
         </Nav>
         <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-danger">Search</Button>
+          <NavLink className={classes.NavLink} to="/">Login</NavLink>
+          <NavLink className={classes.NavLink} to="/">Register</NavLink>
+          {/* <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-danger">Search</Button> */}
         </Form>
       </Navbar.Collapse>
     </Navbar>
