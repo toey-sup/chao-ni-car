47c9b17bc18c0465a148d7194fbffae7da8df827
Kitipat Teachasupinun <Kitipat_teacha@outlook.co.th>
2019-02-06 22:18:58 +0700

    no message

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 const AppWithRouter = () => (
     <BrowserRouter>
       <App />
