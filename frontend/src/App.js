20422f2617985407fe57703e5101651fbd69907b
Ravipas <boss43031@gmail.com>
2019-02-06 16:15:50 +0700

    fixed Master

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import "./App.css";
-import Button from "react-bootstrap/Button";
-import Profile from "./containers/Profile";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./containers/HomePage";
 import AboutPage from "./containers/AboutPage";
@@ -11,12 +9,7 @@ import HowToUsePage from "./containers/HowToUsePage";
 import CarDetail from "./containers/CarDetail/CarDetail";
 import Navbar from "./components/Navbar/Navbar";
 import RegisterPage from "./containers/RegisterPage";
-<<<<<<< HEAD
-import ManageBooking from "./containers/ManageBooking";
-=======
 import AddCarPage from "./containers/AddCarPage/AddCarPage"
->>>>>>> d9f20e4e287f493b7a81ec500a167f095b237ae9
-import LoginComponent from "./components/Login/LoginComponent";
 
 import testPage from "./containers/testPage";
 
@@ -36,11 +29,7 @@ class App extends Component {
           <Route path="/test" component={testPage} />
           <Route path="/howtouse" component={HowToUsePage} />
           <Route path="/regis" component={RegisterPage} />
-<<<<<<< HEAD
-          <Route path="/booking" component={ManageBooking} />
-=======
           <Route path="/addcar" component={AddCarPage} />
->>>>>>> d9f20e4e287f493b7a81ec500a167f095b237ae9
         </Switch>
       </div>
     );
