85d4e4830af47558f77eff63c38ca5390e38af56
Satasuk Viparksinlapin <satasuk@gmail.com>
2019-02-12 12:03:11 +0700

    last update rent[Not complete]

--- frontend/src/containers/Rent/Rent.js
+++ frontend/src/containers/Rent/Rent.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import classes from './Rent.module.css';
-import { Col, Row, FormLabel, FormGroup } from 'react-bootstrap';
+import { Col, Row, FormLabel, FormGroup, FormControl } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 
 class Rent extends Component {
     state = {
         formDate: null,
         toDate: null,
+        formLoc: '',
+        toLoc: '',
         diffDate: null,
     }
 
@@ -25,26 +27,26 @@ class Rent extends Component {
             }
         });
         console.log(this.state);
-
     }
 
     render() {
         return (
             <div className={classes.Div}>
                 <div style={{ textAlign: 'center' }}><h3>Rent</h3></div>
+                <p><i>กรุณากรอกข้อมูลให้ครบทุกช่อง</i></p>
                 {/* เลือกวันเวลาสถานที่ + ชำระเงิน */}
                 <FormGroup>
                     <FormLabel>
                         <strong>Choose Date</strong>
                     </FormLabel>
-                    <Row><Col>{this.state.diffDate ? <div style={{ textAlign: 'center' }}><strong>เป็นเวลา: {this.state.diffDate}วัน</strong></div>: null}</Col></Row>
+                    <Row><Col>{this.state.diffDate ? <div style={{ textAlign: 'center' }}><strong>เป็นเวลา: {this.state.diffDate}วัน</strong></div> : null}</Col></Row>
                     <Row>
                         <Col sm={6}>
                             วันที่จะรับรถ:{" "}
                             <DatePicker
                                 selected={this.state.fromDate}
                                 onChange={date => this.dateChangeHandler(date, "fromDate")}
-                                isClearable
+                                
                             />
                         </Col>
                         <Col sm={6}>
@@ -52,13 +54,33 @@ class Rent extends Component {
                             <DatePicker
                                 selected={this.state.toDate}
                                 onChange={date => this.dateChangeHandler(date, "toDate")}
-                                isClearable
+                                
                             />
                         </Col>
                     </Row>
-
+                    <hr />
                     <Row>
-
+                        <Col sm={12} md={6}><FormLabel>สถานที่ที่ต้องการรับรถ</FormLabel>
+                            <FormControl
+                                size='sm'
+                                type="text"
+                                value={this.state.fromLoc}
+                                placeholder="Enter location"
+                                onChange={(e) => this.onChangeHandler(e, 'fromLoc')}
+                            /></Col>
+                        <Col sm={12} md={6}>
+                            <FormLabel>สถานที่ที่ต้องการส่งรถ</FormLabel>
+                            <FormControl
+                                size='sm'
+                                type="text"
+                                value={this.state.toLoc}
+                                placeholder="Enter location"
+                                onChange={(e) => this.onChangeHandler(e, 'toLoc')}
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col></Col>
                     </Row>
                 </FormGroup>
             </div>
