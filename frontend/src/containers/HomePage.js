dd9db549131510aa1ac5ee78c669fe627cd1be7b
a526735a <31316439+a526735a@users.noreply.github.com>
2019-02-12 15:50:45 +0700

    no message

--- frontend/src/containers/HomePage.js
+++ frontend/src/containers/HomePage.js
@@ -14,75 +14,30 @@ class HomePage extends Component {
     toDate: null,
     loading: false,
     error: null,
-    CARS: [],
     cars: [], //fetch from server
     //====SubQuery======
-    fromLoc: '',
-    toLoc: '',
-    gear: '',
-    seat: 0
-  }
+    fromLoc: "",
+    toLoc: ""
+  };
 
-  componentDidMount() {
-    this.setState({ loading: true })
-    console.log(this.state);
-    const url = '/api/cars?fromDate=' + this.state.fromDate + "&toDate=" + this.state.toDate
-    axios.get(url)
-      .then(res => {
-        console.log("OK")
-        const cars = [...res.data];
-        console.log(cars);
-        this.setState({ cars: [...cars], CARS: [...cars], error: false, loading: false })
-      })
-      .catch(err => {
-        console.log('error')
-        this.setState({ loading: false, error: err })
-      });
-  }
-
-  subQueryHandler = (payload) => {
-    console.log(payload)
+  subQueryHandler = payload => {
+    console.log(payload);
     const newState = {
       ...this.state,
       ...payload
-    }
-    this.setState(newState, () => this.filterItem());
-
+    };
+    this.setState(newState, () => console.log(this.state));
     //Query ต่อ
   };
 
-  filterItem = () => {
-    const updatedCars = this.state.CARS.filter((element) => {
-      if (!this.state.gear == '') {
-        return element.gear.includes(this.state.gear);
-      }
-      if (!this.state.seat == 0) {
-        return element.seat >= this.state.seat;
-      }
-      if (!this.state.fromLoc == '') {
-        //return element.isRented
-        return element.fromLoc.includes(this.state.fromLoc);
-      }
-      if (!this.state.toLoc == '') {
-        return element.toLoc.includes(this.state.toLoc);
-      }
-
-      return true;
-
-    });
-    this.setState({ cars: updatedCars });
-  }
-
-  onChangeHandler = (payload) => {
+  onChangeHandler = payload => {
     const newState = {
       ...this.state,
       fromDate: payload.fromDate,
-      toDate: payload.toDate,
-      gear: payload.gear
-    }
-    this.setState(newState, () => this.searchHandler()) //make search after set the state
-  }
-
+      toDate: payload.toDate
+    };
+    this.setState(newState, () => this.searchHandler()); //make search after set the state
+  };
 
   searchHandler() {
     //ยิง GET request
@@ -94,13 +49,12 @@ class HomePage extends Component {
       "&toDate=" +
       this.state.toDate;
     axios
-      .get(url)
+      .get("/api/cars")
       .then(res => {
         console.log("OK");
         const cars = [...res.data];
         console.log(cars);
-        this.setState({ cars: [...cars], CARS: [...cars], error: false, loading: false })
-
+        this.setState({ cars: [...cars], error: false, loading: false });
       })
       .catch(err => {
         console.log("error");
@@ -127,6 +81,9 @@ class HomePage extends Component {
             <img style={{ width: 80, height: 80 }} src={caricon} />
             <h4>FIND RENTAL CAR </h4>
             <p>Best website</p>
+            <div>
+              <input />
+            </div>
           </div>
           <div className={classes.Filter} style={{ textAlign: "center" }}>
             <QueryFilter change={this.onChangeHandler} />
