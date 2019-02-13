3311e26c3fd6ac28d8884622ad6f3d60123b3f66
Kitipat Teachasupinun <Kitipat_teacha@outlook.co.th>
2019-02-13 19:31:36 +0700

    add carmanagepage(unfinished)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,8 +12,8 @@ import CarDetail from "./containers/CarDetail/CarDetail";
 import Navbar from "./components/Navbar/Navbar";
 import RegisterPage from "./containers/RegisterPage";
 import AddCarPage from "./containers/AddCarPage/AddCarPage"
-
 import testPage from "./containers/test/testPage";
+import carmanage from "./containers/CarManagePage/CarManagePage"
 
 class App extends Component {
   render() {
@@ -33,6 +33,7 @@ class App extends Component {
             <Route path="/regis" component={RegisterPage} />
             <Route path="/addcar" component={AddCarPage} />
             <Route path="/login" component={LoginPage} />
+            <Route path="/carmanage" component = {carmanage}/>
           </Switch>
         </div>
       </div>
