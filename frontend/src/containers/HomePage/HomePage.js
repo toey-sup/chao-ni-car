e6877a2e9ef9733ba6d381ce4665b25ae2ed123e
a526735a <31316439+a526735a@users.noreply.github.com>
2019-04-03 00:36:54 +0700

    add effect to explore button

--- frontend/src/containers/HomePage/HomePage.js
+++ frontend/src/containers/HomePage/HomePage.js
@@ -8,19 +8,25 @@ import Spinner from "../../components/UI/Spinner/Spinner";
 import SubQueryFilter from "../../components/QueryFilter/SubQueryFilter/SubQueryFilter";
 import Banner from "../../images/Banner.png";
 class HomePage extends Component {
-  state = {
-    fromDate: null,
-    toDate: null,
-    loading: false,
-    error: null,
-    CARS: [],
-    cars: [], //fetch from server
-    //====SubQuery======
-    fromLoc: "",
-    toLoc: "",
-    gear: "",
-    seat: 0
-  };
+  constructor() {
+    super();
+    this.state = {
+      fromDate: null,
+      toDate: null,
+      loading: false,
+      error: null,
+      CARS: [],
+      cars: [], //fetch from server
+      //====SubQuery======
+      fromLoc: "",
+      toLoc: "",
+      gear: "",
+      seat: 0,
+      scroll: false
+    };
+    this.buttonHandler = this.buttonHandler.bind(this);
+    this.myDivToFocus = React.createRef();
+  }
 
   componentDidMount() {
     this.setState({ loading: true });
@@ -91,8 +97,13 @@ class HomePage extends Component {
     this.setState(newState, () => this.searchHandler()); //make search after set the state
   };
 
-  buttonHandler(){
-    
+  buttonHandler() {
+    if (this.myDivToFocus.current) {
+      this.myDivToFocus.current.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    }
   }
 
   searchHandler() {
@@ -135,19 +146,21 @@ class HomePage extends Component {
         </div>
       );
     }
-    
+
     return (
       <>
         <div className={classes.Banner}>
-          <div className={centerClass.join(' ')}>
+          <div className={centerClass.join(" ")}>
             <h1 className={classes.texteffect}>RENTAL CAR SERVICE</h1>
             <img style={{ width: "60%", paddingTop: "0" }} src={Banner} />
             <div>
-              <button className={classes.button}>EXPLORE</button>
+              <button className={classes.button} onClick={this.buttonHandler}>
+                EXPLORE
+              </button>
             </div>
           </div>
         </div>
-        <div className={classes.sec2}>
+        <div className={classes.sec2} ref={this.myDivToFocus}>
           <div className={classes.center}>
             <h2>SEARCH</h2>
             <div className={classes.Filter} style={{ textAlign: "center" }}>
