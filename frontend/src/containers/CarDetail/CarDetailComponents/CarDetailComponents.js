e6e8085563c903a46822514b20173f3c97ec4e99
Kitipat Teachasupinun <Kitipat_teacha@outlook.co.th>
2019-04-07 00:31:28 +0700

    change design cardetail (unfinished)

--- frontend/src/containers/CarDetail/CarDetailComponents/CarDetailComponents.js
+++ frontend/src/containers/CarDetail/CarDetailComponents/CarDetailComponents.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Carousel, Row, Col } from 'react-bootstrap';
 import classes from './CarDetailComponents.module.css';
-
+import Rent from '../../Rent/Rent';
 const carPic = (props) => {
     const imagesPath = props.imagesPath.map((path, index) => (
         <Carousel.Item key={index}>
@@ -38,35 +38,40 @@ const carDetailMiddle = (props) => {
         <div className={classes.Detail}>
             <p className={classes.buttomline}></p>
             <Row>
-                <Col>YEAR : </Col><Col>{props.payload.regYear}</Col>
+                <Col  xs="3">YEAR  </Col><Col>: {props.payload.regYear}</Col>
             </Row>
             <Row>
-                <Col>CAR NUMBER : </Col><Col>{props.payload.LNumber}</Col>
+                <Col  xs="3">CAR NUMBER </Col><Col>: {props.payload.LNumber}</Col>
             </Row>
             <Row>
-                <Col>GEAR TYPE : </Col><Col>{props.payload.gear === 'auto' ? 'Auto' : 'Manual'}</Col>
+                <Col  xs="3">GEAR TYPE </Col><Col>: {props.payload.gear === 'auto' ? 'Auto' : 'Manual'}</Col>
             </Row>
             <Row>
-                <Col>SEAT : </Col><Col>{props.payload.seat}</Col>
+                <Col  xs="3">SEAT </Col><Col>: {props.payload.seat}</Col>
             </Row>
             <Row>
-                <Col>EQUIPMENT : </Col><Col>{props.payload.equipment}</Col>
+                <Col  xs="3">EQUIPMENT </Col><Col>: {props.payload.equipment}</Col>
             </Row>
             <Row>
-                <Col>DETAIL : </Col><Col>{props.payload.description}</Col>
+                <Col  xs="3">DETAIL</Col><Col>: {props.payload.description}</Col>
             </Row>
             <Row>
-                <Col>PRICE PER DAY : </Col><Col>{props.payload.pricePerDay}</Col>
+                <Col  xs="3">PRICE PER DAY </Col><Col>: {props.payload.pricePerDay}</Col>
             </Row>
             <Row>
-                <Col>DEPOSIT : </Col><Col>{props.payload.deposit}</Col>
+                <Col  xs="3">DEPOSIT </Col><Col>: {props.payload.deposit}</Col>
             </Row>
             <Row>
-                <Col>AVAILABLE DATE FROM: </Col><Col> {readableDateFrom }  </Col>
+                <Col  xs="3">AVAILABLE FROM </Col><Col>: {readableDateFrom }  </Col>
             </Row>
             <Row>
-                <Col>AVAILABLE DATE UNTIL: </Col><Col> {readableDateTo} </Col>
+                <Col  xs="3">AVAILABLE UNTIL </Col><Col>: {readableDateTo} </Col>
             </Row>
+            <p className={classes.buttomline}></p>
+            <Row>
+               <Rent pricePerDay={props.payload.pricePerDay} deposit={props.payload.deposit} />
+            </Row>
+            
         </div >
     );
 }
