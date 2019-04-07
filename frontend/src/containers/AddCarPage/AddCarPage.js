b983e84a6e43329f69608f52e712edad9884922f
Kitipat Teachasupinun <Kitipat_teacha@outlook.co.th>
2019-04-07 21:05:13 +0700

    change design addcar page

--- frontend/src/containers/AddCarPage/AddCarPage.js
+++ frontend/src/containers/AddCarPage/AddCarPage.js
@@ -72,6 +72,7 @@ class AddCarPage extends Component {
   render() {
     const { validated } = this.state;
     return (
+      <div className={classes.addcarbackground}>
       <div className={classes.wrapper}>
         <p>{this.state.transmission}</p>
         <Form
@@ -79,9 +80,10 @@ class AddCarPage extends Component {
           validated={validated}
           onSubmit={e => this.handleSubmit(e)}
         >
+         <h1>Add Car</h1>
           <Row>
             <Col>
-              <FieldUploadFile addFileURLToState={this.addFileURLToState} />
+              <FieldUploadFile classname = {classes.uploadimgwrapper} addFileURLToState={this.addFileURLToState} />
             </Col>
             <Col>
               <Form.Row>
@@ -117,28 +119,7 @@ class AddCarPage extends Component {
                   />
                 </Form.Group>
               </Form.Row>
-              <Form.Row>
-                <Form.Group as={Col} controlId="seat">
-                  <Form.Label>Seat</Form.Label>
-                  <Form.Control
-                    required
-                    type="Number"
-                    placeholder="Enter Seat"
-                    onChange={this.handleChange}
-                  />
-                </Form.Group>
-              </Form.Row>
 
-              <Form.Row>
-                <Form.Group as={Col} controlId="transmission">
-                  <Form.Label>Transmission</Form.Label>
-                  <Form.Control as="select" onChange={this.handleChange}>
-                    <option>Select</option>
-                    <option value="manual">Manual</option>
-                    <option value="auto">Auto</option>
-                  </Form.Control>
-                </Form.Group>
-              </Form.Row>
 
               {/* <Form.Row>
                 <Form.Group as={Col} controlId="pickupPlace">
@@ -155,7 +136,88 @@ class AddCarPage extends Component {
                 </Form.Group>
               </Form.Row> */}
 
+            </Col>
+          </Row>
+          <Form.Row>
+          <Form.Group as={Col} controlId="deposit">
+                  <Form.Label>Deposit</Form.Label>
+                  <Form.Control
+                    required
+                    type="Number"
+                    placeholder="Enter deposit"
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+                <Form.Group as={Col} controlId="pricePerDay">
+                  <Form.Label>Price per day</Form.Label>
+                  <Form.Control
+                    required
+                    type="Number"
+                    placeholder="Enter pricePerDay"
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+
+          </Form.Row>
+          <Form.Row>
+          <Form.Group as={Col} controlId="seat">
+                  <Form.Label>Seat</Form.Label>
+                  <Form.Control
+                    required
+                    type="Number"
+                    placeholder="Enter Seat"
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+                <Form.Group as={Col} controlId="lnumber">
+                  <Form.Label>License number</Form.Label>
+                  <Form.Control
+                    required
+                    type="String"
+                    placeholder="License number"
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+          </Form.Row>
+          <Form.Row>
+                <Form.Group as={Col} controlId="location">
+                  <Form.Label>Location</Form.Label>
+                  <Form.Control as="select" onChange={this.handleChange}>
+                    <option>Select</option>
+                    <option value="Suvarnabhumi Airport">
+                      Suvarnabhumi Airport
+                    </option>
+                    <option value="Don Mueang Airport">
+                      Don Mueang Airport
+                    </option>
+                    <option value="BTS Morchit">BTS Morchit</option>
+                    <option value="BTS Siam">BTS Siam</option>
+                    <option value="BTS Asok">BTS Asok</option>
+                    <option value="BTS Onnut">BTS Onnut</option>
+                    <option value="BTS Bang Wa">BTS Bang Wa</option>
+                  </Form.Control>
+                </Form.Group>
+                <Form.Group as={Col} controlId="transmission">
+                  <Form.Label>Transmission</Form.Label>
+                  <Form.Control as="select" onChange={this.handleChange}>
+                    <option>Select</option>
+                    <option value="manual">Manual</option>
+                    <option value="auto">Auto</option>
+                  </Form.Control>
+                </Form.Group>
+              </Form.Row>
               <Form.Row>
+                <Form.Group as={Col} controlId="description">
+                  <Form.Label>Description</Form.Label>
+                  <Form.Control
+                    required
+                    type="String"
+                    placeholder="Description"
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+              </Form.Row>
+          <Form.Row>
                 <Form.Group as={Col} controlId="equipment">
                   <Form.Label>Equipment</Form.Label>
                   <Form.Control
@@ -167,19 +229,7 @@ class AddCarPage extends Component {
                 </Form.Group>
               </Form.Row>
 
-              <Form.Row>
-                <Form.Group as={Col} controlId="lnumber">
-                  <Form.Label>License number</Form.Label>
-                  <Form.Control
-                    required
-                    type="String"
-                    placeholder="License number"
-                    onChange={this.handleChange}
-                  />
-                </Form.Group>
-              </Form.Row>
-
-              <Form.Row>
+          <Form.Row>
                 <Form.Group as={Col} controlId="availFrom">
                   <Form.Label>available from</Form.Label>
                   <Form.Control
@@ -199,7 +249,6 @@ class AddCarPage extends Component {
                   />
                 </Form.Group>
               </Form.Row>
-
               <Form.Row>
                 <Form.Group as={Col} controlId="description">
                   <Form.Label>Description</Form.Label>
@@ -212,55 +261,11 @@ class AddCarPage extends Component {
                 </Form.Group>
               </Form.Row>
 
-              <Form.Row>
-                <Form.Group as={Col} controlId="location">
-                  <Form.Label>Location</Form.Label>
-                  <Form.Control as="select" onChange={this.handleChange}>
-                    <option>Select</option>
-                    <option value="Suvarnabhumi Airport">
-                      Suvarnabhumi Airport
-                    </option>
-                    <option value="Don Mueang Airport">
-                      Don Mueang Airport
-                    </option>
-                    <option value="BTS Morchit">BTS Morchit</option>
-                    <option value="BTS Siam">BTS Siam</option>
-                    <option value="BTS Asok">BTS Asok</option>
-                    <option value="BTS Onnut">BTS Onnut</option>
-                    <option value="BTS Bang Wa">BTS Bang Wa</option>
-                  </Form.Control>
-                </Form.Group>
+          <Form.Row className = {classes.buttonwrapper}>
+                <button className ={classes.submit} type="submit">Submit</button>
               </Form.Row>
-
-              <Form.Row>
-                <Form.Group as={Col} controlId="deposit">
-                  <Form.Label>deposit</Form.Label>
-                  <Form.Control
-                    required
-                    type="Number"
-                    placeholder="Enter deposit"
-                    onChange={this.handleChange}
-                  />
-                </Form.Group>
-              </Form.Row>
-              <Form.Row>
-                <Form.Group as={Col} controlId="pricePerDay">
-                  <Form.Label>Price per day</Form.Label>
-                  <Form.Control
-                    required
-                    type="Number"
-                    placeholder="Enter pricePerDay"
-                    onChange={this.handleChange}
-                  />
-                </Form.Group>
-              </Form.Row>
-              <Row>
-                <Button>Cancel</Button>
-                <Button type="submit">Submit</Button>
-              </Row>
-            </Col>
-          </Row>
         </Form>
+      </div>
       </div>
     );
   }
