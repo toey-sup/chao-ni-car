bdf9be3d6e3f40ea2e2bef8d213ad1db8e720a4d
phpunch <nutacp_ryho@hotmail.com>
2019-04-09 22:31:59 +0700

    Update user and car unit test

--- backend/test/car.spec.js
+++ backend/test/car.spec.js
@@ -1,16 +1,57 @@
-const app = require("../index");
+const app = require("../app");
 const request = require("supertest");
+const agent = request.agent(app);
 
+let owner_data = {
+  name: "Punch",
+  surname: "Vit",
+  username: "testUser",
+  password: "123456",
+  email: "nutacp_ryho@hotmail.com",
+  idCardNum: "123123",
+  DLicenseNumber: "123123123",
+  tel: "0810000000",
+  isProvider: true
+};
+let owner_id;
+let car_id;
+beforeEach(async () => {
+  const res = await agent.post("/auth/login").send(owner_data);
+  console.log(res.body._id);
+  owner_id = res.body._id;
+  console.log(owner_id);
+});
 
 describe("Car", function() {
-  let data = {
-    
-  };
-  it("getAllCars", function(done) {
-    request(app)
-      .get("/api/cars")
+  test("addCar", async() => {
+    let data = {
+      brand: "Toyota",
+      type: "Vios",
+      regYear: "1998",
+      LNumber: "Lกฮ999",
+      gear: "auto",
+      seat: "4",
+      equipment: "GPS",
+      photo: "",
+      availFrom: "2019-04-20T00:00:00.000Z",
+      availTo: "2019-04-26T00:00:00.000Z",
+      location: "BTS Siam",
+      description: "TEST CAR",
+      pricePerDay: "300",
+      deposit: "1000",
+      ownerTestId: owner_id
+    };
+    const res = await request(app)
+      .post("/api/cars")
+      .send(data)
+      .set("Accept", "application/json").expect(200)
+    car_id = res.body._id
+    return res
+  });
+  test("getCar", () => {
+    return request(app)
+      .get("/api/cars/" + car_id)
       .set("Accept", "application/json")
-      .expect(200, done);
+      .expect(200);
   })
-  
-})
+});
--- backend/test/user.spec.js
+++ backend/test/user.spec.js
@@ -1,33 +1,39 @@
-const app = require("../index");
+const app = require("../app");
 const request = require("supertest");
+const agent = request.agent(app);
 
+let owner_data = {
+  name: "Punch",
+  surname: "Vit",
+  username: "testUser",
+  password: "123456",
+  email: "nutacp_ryho@hotmail.com",
+  idCardNum: "123123",
+  DLicenseNumber: "123123123",
+  tel: "0810000000",
+  isProvider: true
+};
+let owner_id;
+beforeEach(async () => {
+  const res = await agent.post("/auth/login").send(owner_data);
+  owner_id = res.body._id
+});
 
 describe("User", function() {
-  let data = {
-    name: "Punch",
-      surname: "Vit",
-      username: "testUser",
-      password: "123456",
-      email: "nutacp_ryho@hotmail.com",
-      idCardNum: "123123",
-      DLicenseNumber: "123123123",
-      tel: "0814904388",
-      isProvider: true
-  };
-  it("sign up", function(done) {
-    request(app)
-      .post("/auth/local")
-      .send(data)
-      .set("Accept", "application/json")
-      .expect(200, { message: 'Sign Up Success' }, done);
-  })
-  it("loginSuccessful", function(done) {
-    request(app)
-      .post("/auth/login")
-      .send(data)
-      .set("Accept", "application/json")
-      .expect(200, done);
-  })
+  // it("sign up", function(done) {
+  //   request(app)
+  //     .post("/auth/local")
+  //     .send(data)
+  //     .set("Accept", "application/json")
+  //     .expect(200, { message: 'Sign Up Success' }, done);
+  // })
+  // it("loginSuccessful", (done) => {
+  //   request(app)
+  //     .post("/auth/login")
+  //     .send(data)
+  //     .set("Accept", "application/json")
+  //     .expect(200, done);
+  // })
   it("loginFailure", function(done) {
     data = {
       username: "??",
@@ -39,4 +45,19 @@ describe("User", function() {
       .set("Accept", "application/json")
       .expect(401, done);
   })
-})
+  it("getUserProfile", async () => {
+    const res = await agent
+      .get(`/api/current_user`)
+      .expect(200);
+    return res
+  });
+  // it("getCurrentUser", function(done) {
+  //   console.log(session);
+  //   agent
+  //     .get("/api/current_user")
+  //     .send(data)
+  //     .set("Cookie", session)
+  //     .set("Accept", "application/json")
+  //     .expect(200, done);
+  // });
+});
