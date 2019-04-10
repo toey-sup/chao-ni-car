8b3421c2d6e5f9f5215d821682287afa9973b00d
Kitipat Teachasupinun <Kitipat_teacha@outlook.co.th>
2019-04-10 14:51:00 +0700

    fix design Car component

--- frontend/src/components/Ownercar/Car.js
+++ frontend/src/components/Ownercar/Car.js
@@ -2,7 +2,7 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import binicon from "../../images/binicon.png";
 import editicon from "../../images/editicon.png";
-import { Button, Modal, ModalBody ,Card} from "react-bootstrap";
+import { Button, Modal, ModalBody, Card, Col, Row } from "react-bootstrap";
 import "./Car.css";
 import TextTruncate from "react-text-truncate";
 class Car extends React.Component {
@@ -23,6 +23,7 @@ class Car extends React.Component {
 
   render() {
     return (
+      /*
       <div className="car">
         <div
           style={{
@@ -97,6 +98,80 @@ class Car extends React.Component {
             />
           </div>
         </div>
+
+        <Modal show={this.state.show && this.props.isRented}>
+          <Modal.Header>
+            <Modal.Title>Alert</Modal.Title>
+          </Modal.Header>
+          <Modal.Footer>
+            <p style={{ marginRight: "auto" }}>
+              {this.props.brand} {this.props.LNumber} is being rented
+            </p>
+            <Button
+              variant="secondary"
+              onClick={() => this.setState({ show: false })}
+            >
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        <Modal show={this.state.show && !this.props.isRented}>
+          <Modal.Header>
+            <Modal.Title>Alert</Modal.Title>
+          </Modal.Header>
+          <Modal.Footer>
+            <p style={{ marginRight: "auto" }}>
+              Are you sure to remove {this.props.brand} {this.props.LNumber} ?
+            </p>
+            <Button variant="outline-danger" onClick={this.handleDelete}>
+              Remove
+            </Button>
+            <Button
+              variant="secondary"
+              onClick={() => this.setState({ show: false })}
+            >
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+        */
+      <div>
+        <Card.Header as="h5" className="detail">
+          Car Id : {this.props.id}
+        </Card.Header>
+        <Card.Body>
+          <Row>
+            <Col xs={6} md={4}>
+              <Card.Img src={this.props.picture} className="imagecar" />
+            </Col>
+            <Col xs={6} md={4}>
+              {this.props.brand} {this.props.type}
+              <div className="detail">
+                <p>
+                  license : {this.props.LNumber}<br />
+                  Register Year : {this.props.regYear}<br />
+                  Price : {this.props.pricePerDay} ฿/Day<br />
+                  
+                  
+
+                </p>
+              </div>
+            </Col>
+            <Col>
+              <button
+                className="delete"
+                onClick={() => this.setState({ show: true })}
+              >
+                Delete
+              </button>
+            </Col>
+          </Row>
+        </Card.Body>
+
+
+
+
         <Modal show={this.state.show && this.props.isRented}>
           <Modal.Header>
             <Modal.Title>Alert</Modal.Title>
