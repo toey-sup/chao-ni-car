fd0441775b57d846235acf2ac144a5a919617fcc
phpunch <nutacp_ryho@hotmail.com>
2019-04-24 20:25:06 +0700

    Fix Navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -5,7 +5,6 @@ import classes from "./Navbar.module.css";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import logo from "./logo2.png";
-import DropdownMenu from "react-bootstrap/DropdownMenu";
 
 class NavbarComponent extends Component {
   constructor(props) {
@@ -46,7 +45,7 @@ class NavbarComponent extends Component {
         console.log("Logout");
         display = (
           <Form inline>
-            <Nav>
+            
               <NavLink
                 className={classes.NavLink}
                 style={{
@@ -56,21 +55,21 @@ class NavbarComponent extends Component {
               >
                 {this.props.user.name}
               </NavLink>
-            </Nav>
+            
             {this.props.user.isProvider ? (
-              <Nav>
+              
                 <NavLink className={classes.NavLink} to="/carmanage">
                   CARMANAGE
                 </NavLink>
-              </Nav>
+              
             ) : null}
 
-            <Nav>
+            
               <NavLink className={classes.NavLink} to="/managebooking">
                 BOOKING
               </NavLink>
-            </Nav>
-            <Nav>
+            
+            
               <NavLink
                 className={classes.NavLink}
                 to="/"
@@ -80,23 +79,9 @@ class NavbarComponent extends Component {
               >
                 LOGOUT
               </NavLink>
-            </Nav>
+            
           </Form>
         );
-        /*
-        menu = (
-          <DropdownMenu>
-            <Dropdown.Item href="/">HOME</Dropdown.Item>
-            <Dropdown.Item href="/about">ABOUT</Dropdown.Item>
-            <Dropdown.Item href="/howtouse">HOWTO</Dropdown.Item>
-            {this.props.user.isProvider ? (
-              <Dropdown.Item href="/carmanage">CARMANAGE</Dropdown.Item>
-            ) : null}
-            <Dropdown.Item href="/managebooking">BOOKING</Dropdown.Item>
-            <Dropdown.Item href="/logout">LOGOUT</Dropdown.Item>
-          </DropdownMenu>
-        );
-        */
         break;
     }
     return (
@@ -112,14 +97,7 @@ class NavbarComponent extends Component {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link
-              style={{
-                color: "white"
-              }}
-              href="/about"
-            >
-              ABOUT
-            </Nav.Link>
+
             <Nav.Link
               style={{
                 color: "white"
@@ -127,6 +105,14 @@ class NavbarComponent extends Component {
               href="/howtouse"
             >
               HOWTO
+            </Nav.Link>
+            <Nav.Link
+              style={{
+                color: "white"
+              }}
+              href="/about"
+            >
+              ABOUT
             </Nav.Link>
           </Nav>
           <Nav>{display}</Nav>
